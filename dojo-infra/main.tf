41a44d8db2f9aa6a12c5252bcbd9dc4ed50e8fbc
Henrique Westphal <henriquewalves@gmail.com>
2025-04-07 22:35:27 -0300

    add backend s3

--- dojo-infra/main.tf
+++ dojo-infra/main.tf
@@ -1,3 +1,18 @@
 provider "aws" {
   region  = var.region
 }
+
+terraform {
+  backend "s3" {
+    bucket   = "dojo-management-tfstate"
+    key      = "dojo-infra/${var.project_name}-${var.env}-tfstate"
+    region   = "sa-east-1"
+    endpoints = {
+      s3 = "https://oss.sa-east-1.prod-cloud-ocb.orange-business.com"
+    }
+
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+  }
+}
