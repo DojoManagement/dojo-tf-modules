1c2cbd4c16555a27133850ff3cf14884296ab001
Henrique Westphal <henriquewalves@gmail.com>
2025-05-26 14:50:04 -0300

    test

--- dojo-lambda/s3.tf
+++ dojo-lambda/s3.tf
@@ -5,6 +5,6 @@ data "aws_s3_bucket" "selected" {
 resource "aws_s3_bucket_object" "lambda_zip" {
   bucket = data.aws_s3_bucket.selected.id
   key    = "lambda_build/${var.lambda_name}/lambda_package.zip"
-  source = "lambda_build/lambda_package.zip"
-  etag   = filemd5("lambda_build/lambda_package.zip")
+  source = "../lambda_build/lambda_package.zip"
+  etag   = filemd5("../lambda_build/lambda_package.zip")
 }
