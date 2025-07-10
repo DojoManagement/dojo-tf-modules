a8d0385a1b214e325a9bc64b288feab0374eb8fa
Henrique Westphal <henriquewalves@gmail.com>
2025-07-10 15:21:03 -0300

    change variable type and recreate resources

--- dojo-lambda/api-gateway.tf
+++ dojo-lambda/api-gateway.tf
@@ -1,5 +1,16 @@
+locals {
+  method_map = flatten([
+    for route, methods in var.routes : [
+      for method in methods : {
+        route  = route
+        method = method
+      }
+    ]
+  ])
+}
+
 resource "aws_api_gateway_resource" "this" {
-  for_each = { for route in var.routes : "${route.path_part}" => route }
+  for_each = var.routes
 
   rest_api_id = data.aws_api_gateway_rest_api.api_gateway.id
   parent_id   = data.aws_api_gateway_rest_api.api_gateway.root_resource_id
@@ -7,20 +18,20 @@ resource "aws_api_gateway_resource" "this" {
 }
 
 resource "aws_api_gateway_method" "this" {
-  for_each = { for route in var.routes : "${route.method}#${route.path_part}" => route }
+  for_each = { for item in local.method_map : "${item.route}_${item.method}" => item }
 
   rest_api_id   = data.aws_api_gateway_rest_api.api_gateway.id
-  resource_id   = aws_api_gateway_resource.this["${each.value.path_part}"].id
+  resource_id   = aws_api_gateway_resource.this["${each.value.route}"].id
   http_method   = each.value.method
   authorization = "NONE"
 }
 
 resource "aws_api_gateway_integration" "this" {
-  for_each = { for route in var.routes : "${route.method}#${route.path_part}" => route }
+  for_each = aws_api_gateway_method.this
 
-  rest_api_id = data.aws_api_gateway_rest_api.api_gateway.id
-  resource_id = aws_api_gateway_resource.this["${each.value.path_part}"].id
-  http_method = aws_api_gateway_method.this["${each.value.method}#${each.value.path_part}"].http_method
+  rest_api_id = each.value.rest_api_id
+  resource_id = each.value.resource_id
+  http_method = each.value.http_method
   integration_http_method = "POST"
   type = "AWS_PROXY"
   uri = aws_lambda_function.lambda_function.invoke_arn
