b9e97d11a4bd8661d2e1c6d346819c881d7fbaf7
Henrique Westphal <henriquewalves@gmail.com>
2025-07-10 14:21:15 -0300

    fix to create one

--- dojo-lambda/api-gateway.tf
+++ dojo-lambda/api-gateway.tf
@@ -1,5 +1,5 @@
 resource "aws_api_gateway_resource" "this" {
-  for_each = { for route in var.routes : "${route.method}#${route.path_part}" => route }
+  for_each = { for route in var.routes : "${route.path_part}" => route }
 
   rest_api_id = data.aws_api_gateway_rest_api.api_gateway.id
   parent_id   = data.aws_api_gateway_rest_api.api_gateway.root_resource_id
@@ -19,7 +19,7 @@ resource "aws_api_gateway_integration" "this" {
   for_each = { for route in var.routes : "${route.method}#${route.path_part}" => route }
 
   rest_api_id = data.aws_api_gateway_rest_api.api_gateway.id
-  resource_id = aws_api_gateway_resource.this["${each.value.method}#${each.value.path_part}"].id
+  resource_id = aws_api_gateway_resource.this["${each.value.path_part}"].id
   http_method = aws_api_gateway_method.this["${each.value.method}#${each.value.path_part}"].http_method
   integration_http_method = "POST"
   type = "AWS_PROXY"
