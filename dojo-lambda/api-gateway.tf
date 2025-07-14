167b30e1a5a43f3e808d40dd34dc333f6db0e450
Henrique Westphal <henriquewalves@gmail.com>
2025-07-14 16:08:51 -0300

    test to create sub path

--- dojo-lambda/api-gateway.tf
+++ dojo-lambda/api-gateway.tf
@@ -7,14 +7,29 @@ locals {
       }
     ]
   ])
+  all_paths = distinct(flatten([
+    for path, methods in var.routes : [
+      [for i in range(1, length(split("/", path)) + 1) :
+        join("/", slice(split("/", path), 0, i))
+      ]
+    ]
+  ]))
 }
 
 resource "aws_api_gateway_resource" "this" {
-  for_each = var.routes
+  for_each = { for path in local.all_paths : path => path }
 
   rest_api_id = data.aws_api_gateway_rest_api.api_gateway.id
-  parent_id   = data.aws_api_gateway_rest_api.api_gateway.root_resource_id
-  path_part   = each.key
+
+  parent_id = (
+    length(split("/", each.key)) == 1
+    ? data.aws_api_gateway_rest_api.api_gateway.root_resource_id
+    : aws_api_gateway_resource.this[
+        join("/", slice(split("/", each.key), 0, length(split("/", each.key)) - 1))
+      ].id
+  )
+
+  path_part = element(reverse(split("/", each.key)), 0)
 }
 
 resource "aws_api_gateway_method" "this" {
@@ -27,11 +42,11 @@ resource "aws_api_gateway_method" "this" {
 }
 
 resource "aws_api_gateway_integration" "this" {
-  for_each = aws_api_gateway_method.this
+  for_each = { for item in local.method_map : "${item.route}_${item.method}" => item }
 
-  rest_api_id = each.value.rest_api_id
-  resource_id = each.value.resource_id
-  http_method = each.value.http_method
+  rest_api_id = data.aws_api_gateway_rest_api.api_gateway.id
+  resource_id = aws_api_gateway_resource.this[item.path].id
+  http_method = aws_api_gateway_method.this["${item.route}_${item.method}"].http_method
   integration_http_method = "POST"
   type = "AWS_PROXY"
   uri = aws_lambda_function.lambda_function.invoke_arn
